1c558ad64f7a407f79e63ecad552dccafad94a63
gustavogoncc <105744484+gustavogoncc@users.noreply.github.com>
2023-05-17 22:23:07 -0300

    Create 1181BEE.c

---
+++ 1181BEE.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main(){
+    setlocale(LC_ALL,"Portuguese");
+    float M[12][12], soma = 0, media = 0, elemento;
+    int i, j, linha;
+    char operacao;
+
+    scanf("%d %c",&linha,&operacao);
+
+    for(i=0; i<12; i++);
+
+    for(j=0; j<12; j++);
+
+    scanf("%f",&elemento);
+     m[i][j] = elemento;
+
+    if(opera��o = 'S');
+    e = linha;
+    while(i == linha);
+
+     for(j=0; j=<12; j++);
+      soma += m[i][j];
+      i++;
+
+
+
+
+
+
+
+
+
+
+
+}
