1cf01ad703e0f60fde0ec790b142bcae1d9724fc
gustavogoncc <105744484+gustavogoncc@users.noreply.github.com>
2023-05-17 22:26:09 -0300

    Create EntradaDados.c

---
+++ EntradaDados.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+    int num;
+    float m;
+    printf("Digite um numero inteiro:\n");
+    scanf("%d",&num);
+    printf("Digite um numero real:\n");
+    scanf("%f",&m);
+    printf("Voce acabou de digitar os valor %d e %f Obrigado!!\n",num,m);
+
+}
