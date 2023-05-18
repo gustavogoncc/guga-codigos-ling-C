bdf3f0d33cfd307ea4b6c9de5214aa109287a206
gustavogoncc <105744484+gustavogoncc@users.noreply.github.com>
2023-05-17 22:20:27 -0300

    Create OperacaoStrings.c

---
+++ OperacaoStrings.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+void main(){
+    char sit[10];
+    float media = 10.0;
+    strcpy(sit,(media>=7)?"Aprovado":"Reprovado");
+    printf("A situacao do aluno e %s",sit);
+
+
+
+
+
+
+
+
+}
