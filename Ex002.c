6b99467bc4314e1c3d629d55f4122e1ea2aa7d77
gustavogoncc <105744484+gustavogoncc@users.noreply.github.com>
2023-05-17 22:29:14 -0300

    Create Ex002.c

---
+++ Ex002.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+void main(){
+    setlocale(LC_ALL, "Portuguese");
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+    printf("\\a \t=\t beep\n");
+    printf("\\n \t=\t nova linha \n");
+    printf("\\t \t=\t tabula��o\n");
+    printf("\\\\ \t=\t barra\n");
+    printf("%%%% \t=\t porcentagem\n");
+    printf("\\\? \t=\t interroga��o\n");
+    printf("=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=\n");
+
+
+}
