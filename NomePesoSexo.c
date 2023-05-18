afff52b5b5d95fb103f7b7922e1e4e3b6f04b4fd
gustavogoncc <105744484+gustavogoncc@users.noreply.github.com>
2023-05-17 22:27:16 -0300

    Create NomePesoSexo.c

---
+++ NomePesoSexo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(){
+    printf("O %s tem %i anos de idade\n","Gustavo",18);
+    printf("Seu peso atual e %2.fkg\n",90.1);
+    printf("O seu sexo e %C",'M');
+
+
+
+}
