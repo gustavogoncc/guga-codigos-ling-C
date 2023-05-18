ebabaf442d84fb210b395080805d8e48169e5bba
gustavogoncc <105744484+gustavogoncc@users.noreply.github.com>
2023-05-17 22:21:56 -0300

    Create NumerosAleatorios.c

---
+++ NumerosAleatorios.c
@@ -0,0 +1,16 @@
+#import <stdio.h>
+#import <stdlib.h>
+#import <time.h>
+
+void main(){
+    srand(time(NULL));
+    int n;
+    n = rand()%10; //Gera entre 0 e 9
+    printf("Eu gerei o numero %d",n);
+
+
+
+
+
+
+}
