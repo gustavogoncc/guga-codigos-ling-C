757417bdf5114efeb74b8db9a10983341ca539b7
gustavogoncc <105744484+gustavogoncc@users.noreply.github.com>
2023-05-17 22:30:28 -0300

    Create variaveis.c

---
+++ variaveis.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(){
+    unsigned int idade = 18;
+    float peso = 90.5;
+    char sexo = 'M';
+    char nome[] = "Gustavo Goncalves";
+
+    printf("%s e do sexo %c, tem %d anos e pesa %fkg",nome,sexo,idade, peso);
+}
